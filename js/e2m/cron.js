310c1a023c5ab79c18061685972931af42835ab7
roma_bb8 <jungle.romabb8@gmail.com>
2020-02-16 11:32:51 +0200

    cron run

--- js/e2m/cron.js
+++ js/e2m/cron.js
@@ -1,31 +1,24 @@
-(function () {
-    function runCron() {
-        new Ajax.Request(e2M.url.cronURL, {
-            method: 'get',
-            onCreate: function () {
-                $('loading-mask').setStyle({
-                    visibility: 'hidden'
-                });
-            },
-            onComplete: function(transport) {
-                console.log(transport);
-            },
-            onSuccess: function (transport) {
-                var response = JSON.parse(transport.responseText);
+function cron() {
+    new Ajax.Request(e2M.url.cronURL, {
+        method: 'get',
+        onCreate: function () {
+            $('loading-mask').setStyle({
+                visibility: 'hidden'
+            });
+        },
+        onSuccess: function (transport) {
+            var response = JSON.parse(transport.responseText);
 
-                console.log(response);
+            response.run && setTimeout(cron, 3000);
 
-                response.run && setTimeout(runCron, 5000);
-                response.handlers.forEach(function (data) {
-                    data.handler === 'downloadInventoryHandler' && downloadInventoryHandler(data.data);
-                    data.handler === 'importInventoryHandler' && importInventoryHandler(data.data);
-                });
-            },
-            onFailure: function () {
-                alert('cron tasks did not complete successfully...');
-            }
-        });
-    }
-
-    setTimeout(runCron, 100);
-})();
+            response.handlers.forEach(function (data) {
+                data.handler === 'downloadInventoryHandler' && downloadInventoryHandler(data.data);
+                data.handler === 'importInventoryHandler' && importInventoryHandler(data.data);
+            });
+        },
+        onFailure: function (transport) {
+            console.log(transport);
+            alert('Cron tasks did not complete successfully...');
+        }
+    });
+}
