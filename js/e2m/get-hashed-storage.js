360a0c3d20d8b92607515dfef30e8aa458490c22
roma_bb8 <jungle.romabb8@gmail.com>
2020-02-16 21:13:24 +0200

    c

---
+++ js/e2m/get-hashed-storage.js
@@ -0,0 +1,9 @@
+function getHashedStorage(id) {
+
+    var hashedStorageKey = e2m.prefix + md5(id).substr(0, 10);
+    if (typeof e2m.localStorage[hashedStorageKey] === 'undefined') {
+        return '';
+    }
+
+    return e2m.localStorage[hashedStorageKey];
+}
