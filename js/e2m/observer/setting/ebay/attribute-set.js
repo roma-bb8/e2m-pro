aeb5aca65ca76003d4466f71b5150796552cba87
roma_bb8 <jungle.romabb8@gmail.com>
2020-02-16 20:53:39 +0200

    c

---
+++ js/e2m/observer/setting/ebay/attribute-set.js
@@ -0,0 +1,42 @@
+(function () {
+    'use strict';
+
+    Event.observe(window, 'load', function () {
+        $('attribute-set1').observe('change', function (element) {
+            new Ajax.Request(e2m.url.getAttributsBySet, {
+                method: 'get',
+                parameters: {
+                    set_id: element.target.options[element.target.selectedIndex].value
+                },
+                onSuccess: function (transport) {
+                    var response = JSON.parse(transport.responseText);
+
+                    e2m.magentoAllAttributes = response.attributes;
+                    e2m.fieldAttribute = {};
+
+                    paintImportProperties();
+
+                    var magentoAttribute = $('magento-attribute');
+
+                    magentoAttribute.select('option').invoke('remove');
+                    var cleanOption = document.createElement('option');
+                    cleanOption.text = '';
+                    magentoAttribute.add(cleanOption);
+
+                    for (var [code, title] of Object.entries(e2m.magentoAllAttributes)) {
+                        var option = document.createElement('option');
+                        option.id = code;
+                        option.text = title.toString();
+                        magentoAttribute.add(option);
+                    }
+                },
+                onFailure: function (transport) {
+                    var response = JSON.parse(transport.responseText);
+                    console.log(response);
+
+                    alert('Something went wrong...');
+                }
+            });
+        });
+    }, false);
+})();
