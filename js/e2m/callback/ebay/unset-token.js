1e4e6ef524b9f21bf6bd7f95c0c52f6a2b78b9ab
roma_bb8 <jungle.romabb8@gmail.com>
2020-02-16 19:03:44 +0200

    c

---
+++ js/e2m/callback/ebay/unset-token.js
@@ -0,0 +1,15 @@
+function unsetToken() {
+    new Ajax.Request(e2m.url.deleteEbayToken, {
+            method: 'get',
+            onSuccess: function () {
+                window.location.reload();
+            },
+            onFailure: function (transport) {
+                console.log(transport);
+
+                alert('Something went wrong...');
+                return false;
+            }
+        }
+    );
+}
