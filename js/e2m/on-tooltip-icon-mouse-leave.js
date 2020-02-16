8f742d606a7e283e82a70847ace81617233fc48c
roma_bb8 <jungle.romabb8@gmail.com>
2020-02-16 21:31:59 +0200

    c

--- js/e2m/on-tooltip-icon-mouse-leave.js
+++ js/e2m/on-tooltip-icon-mouse-leave.js
@@ -2,7 +2,8 @@ function onToolTipIconMouseLeave() {
 
     e2m.isHideTooltip = true;
 
+    var element = this.up().select('.tool-tip-message')[0];
     setTimeout(function () {
-        e2m.isHideTooltip && this.up().select('.tool-tip-message')[0].hide();
+        e2m.isHideTooltip && element.hide();
     }, 1000);
 }
