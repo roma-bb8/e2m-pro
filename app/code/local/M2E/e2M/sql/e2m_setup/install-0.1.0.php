5dd8a6bb84d9973dda80e246f76ec45e7b02c21e
roma_bb8 <jungle.romabb8@gmail.com>
2020-02-16 23:09:59 +0200

    c

--- app/code/local/M2E/e2M/sql/e2m_setup/install-0.1.0.php
+++ app/code/local/M2E/e2M/sql/e2m_setup/install-0.1.0.php
@@ -6,8 +6,8 @@ $installer = $this;
 //########################################
 
 $sql = <<<SQL
-DROP TABLE IF EXISTS `m2e_e2m_cron_tasks_in_processing`;
-CREATE TABLE `m2e_e2m_cron_tasks_in_processing` (
+DROP TABLE IF EXISTS `{$installer->getTable('m2e_e2m_cron_tasks_in_processing')}`;
+CREATE TABLE `{$installer->getTable('m2e_e2m_cron_tasks_in_processing')}` (
   `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `is_running` BOOL NOT NULL DEFAULT FALSE,
   `instance` VARCHAR(255) NOT NULL,
@@ -18,8 +18,8 @@ CREATE TABLE `m2e_e2m_cron_tasks_in_processing` (
   UNIQUE KEY (`instance`)
 ) ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;
 
-DROP TABLE IF EXISTS `m2e_e2m_inventory_ebay`;
-CREATE TABLE `m2e_e2m_inventory_ebay` (
+DROP TABLE IF EXISTS `{$installer->getTable('m2e_e2m_inventory_ebay')}`;
+CREATE TABLE `{$installer->getTable('m2e_e2m_inventory_ebay')}` (
   `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `marketplace_id` SMALLINT NOT NULL,
   `item_id` DECIMAL(20) UNSIGNED NOT NULL,
@@ -33,7 +33,19 @@ CREATE TABLE `m2e_e2m_inventory_ebay` (
   INDEX `variation` (`variation`)
 ) ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;
 
-INSERT INTO `m2e_e2m_cron_tasks_in_processing` (`instance`, `data`) VALUES ('Cron_Task_Completed', '{}');
+DROP TABLE IF EXISTS `{$installer->getTable('m2e_e2m_log')}`;
+CREATE TABLE `{$installer->getTable('m2e_e2m_log')}` (
+  `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `description` TEXT DEFAULT NULL,
+  `updated` TIMESTAMP DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `created` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+INSERT INTO `{$installer->getTable('m2e_e2m_cron_tasks_in_processing')}`
+    (`instance`, `data`)
+VALUES
+    ('Cron_Task_Completed', '{}');
 SQL;
 
 $installer->run($sql);
