2cf941c0525fbbeca7bd115a4b1fb79fe0e02012
roma_bb8 <jungle.romabb8@gmail.com>
2020-02-17 13:23:05 +0200

    c

--- app/code/local/M2E/e2M/Helper/eBay/Account.php
+++ app/code/local/M2E/e2M/Helper/eBay/Account.php
@@ -1,17 +1,20 @@
 <?php
 
+/**
+ * Class M2E_e2M_Helper_eBay_Account
+ */
 class M2E_e2M_Helper_eBay_Account {
-
-    const MODE_SANDBOX = 0;
-    const MODE_PRODUCTION = 1;
 
     const PREFIX = M2E_e2M_Helper_Data::PREFIX . 'ebay/account/';
 
+    const MODE_PRODUCTION = 1;
+    const MODE_SANDBOX = 2;
+
+    const SESSION_ID = 'session_id';
     const MODE = 'mode';
     const TOKEN = 'token';
     const EXPIRATION_TIME = 'expiration_time';
     const USER_ID = 'user_id';
-    const SESSION_ID = 'session_id';
 
     //########################################
 
@@ -26,6 +29,81 @@ class M2E_e2M_Helper_eBay_Account {
     //########################################
 
     /**
+     * @param int $mode
+     *
+     * @return $this
+     */
+    public function setMode($mode) {
+        $this->data['mode'] = $mode;
+
+        return $this;
+    }
+
+    /**
+     * @param string $sessionId
+     *
+     * @return $this
+     */
+    public function setSessionId($sessionId) {
+        $this->data['session_id'] = $sessionId;
+
+        return $this;
+    }
+
+    /**
+     * @param string $token
+     *
+     * @return $this
+     */
+    public function setToken($token) {
+        $this->data['token'] = $token;
+
+        return $this;
+    }
+
+    /**
+     * @param string $expirationTime
+     *
+     * @return $this
+     */
+    public function setExpirationTime($expirationTime) {
+        $this->data['expiration_time'] = $expirationTime;
+
+        return $this;
+    }
+
+    /**
+     * @param int $userId
+     *
+     * @return $this
+     */
+    public function setUserId($userId) {
+        $this->data['user_id'] = $userId;
+
+        return $this;
+    }
+
+    //----------------------------------------
+
+    /**
+     * @param array $data
+     *
+     * @return $this
+     */
+    public function setData($data) {
+
+        isset($data['mode']) && $this->setMode($data['mode']);
+        isset($data['session_id']) && $this->setSessionId($data['session_id']);
+        isset($data['token']) && $this->setToken($data['token']);
+        isset($data['expiration_time']) && $this->setExpirationTime($data['expiration_time']);
+        isset($data['user_id']) && $this->setUserId($data['user_id']);
+
+        return $this;
+    }
+
+    //########################################
+
+    /**
      * @return int
      */
     public function getMode() {
@@ -35,71 +113,76 @@ class M2E_e2M_Helper_eBay_Account {
     /**
      * @return bool
      */
-    public function isProduction() {
+    public function isProductionMode() {
         return self::MODE_PRODUCTION === $this->getMode();
     }
 
-    public function getToken() {
-        return $this->data['token'];
+    /**
+     * @return bool
+     */
+    public function isSandboxMode() {
+        return self::MODE_SANDBOX === $this->getMode();
     }
 
-    public function getExpirationTime() {
-        return $this->data['expiration_time'];
-    }
-
-    public function getUserId() {
-        return $this->data['user_id'];
-    }
-
+    /**
+     * @return string|null
+     */
     public function getSessionId() {
         return $this->data['session_id'];
     }
 
-    public function setMode($mode) {
-        $this->data['mode'] = $mode;
+    /**
+     * @return string|null
+     */
+    public function getToken() {
+        return $this->data['token'];
     }
 
-    public function setToken($token) {
-        $this->data['token'] = $token;
+    /**
+     * @return string|null
+     */
+    public function getUserId() {
+        return $this->data['user_id'];
     }
 
-    public function setExpirationTime($expirationTime) {
-        $this->data['expiration_time'] = $expirationTime;
+    /**
+     * @return string
+     */
+    public function getAccountUrl() {
+
+        $domain = 'ebay.com';
+        if ($this->isSandboxMode()) {
+            $domain = 'sandbox.' . $domain;
+        }
+
+        return 'http://' . $domain . '/' . (string)$this->getUserId();
     }
 
-    public function setUserId($userId) {
-        $this->data['user_id'] = $userId;
-    }
+    //########################################
 
-    public function setSessionId($sessionId) {
-        $this->data['session_id'] = $sessionId;
-    }
-
-    public function setData($data) {
-
-        isset($data['mode']) && $this->setMode($data['mode']);
-        isset($data['token']) && $this->setToken($data['token']);
-        isset($data['expiration_time']) && $this->setExpirationTime($data['expiration_time']);
-        isset($data['user_id']) && $this->setUserId($data['user_id']);
-        isset($data['session_id']) && $this->setSessionId($data['session_id']);
-    }
-
+    /**
+     * @return $this
+     */
     public function save() {
 
         $connWrite = $this->resource->getConnection('core_write');
 
         $connWrite->delete($this->coreConfigDataTableName, array('path IN (?)' => array(
             self::PREFIX . self::MODE,
+            self::PREFIX . self::SESSION_ID,
             self::PREFIX . self::TOKEN,
             self::PREFIX . self::EXPIRATION_TIME,
-            self::PREFIX . self::USER_ID,
-            self::PREFIX . self::SESSION_ID
+            self::PREFIX . self::USER_ID
         )));
 
         $connWrite->insertMultiple($this->coreConfigDataTableName, array(
             array(
                 'path' => self::PREFIX . self::MODE,
                 'value' => $this->data['mode']
+            ),
+            array(
+                'path' => self::PREFIX . self::SESSION_ID,
+                'value' => $this->data['session_id']
             ),
             array(
                 'path' => self::PREFIX . self::TOKEN,
@@ -112,16 +195,17 @@ class M2E_e2M_Helper_eBay_Account {
             array(
                 'path' => self::PREFIX . self::USER_ID,
                 'value' => $this->data['user_id']
-            ),
-            array(
-                'path' => self::PREFIX . self::SESSION_ID,
-                'value' => $this->data['session_id']
             )
         ));
 
         return $this;
     }
 
+    //########################################
+
+    /**
+     * M2E_e2M_Helper_eBay_Account constructor.
+     */
     public function __construct() {
 
         $this->resource = Mage::getSingleton('core/resource');
@@ -130,11 +214,11 @@ class M2E_e2M_Helper_eBay_Account {
         //----------------------------------------
 
         $this->data = array(
-            'mode' => 0,
+            'mode' => self::MODE_SANDBOX,
+            'session_id' => null,
             'token' => null,
             'expiration_time' => null,
-            'user_id' => null,
-            'session_id' => null
+            'user_id' => null
         );
 
         //----------------------------------------
@@ -143,10 +227,10 @@ class M2E_e2M_Helper_eBay_Account {
             ->from($this->coreConfigDataTableName)
             ->where('path IN (?)', array(
                 self::PREFIX . self::MODE,
+                self::PREFIX . self::SESSION_ID,
                 self::PREFIX . self::TOKEN,
                 self::PREFIX . self::EXPIRATION_TIME,
-                self::PREFIX . self::USER_ID,
-                self::PREFIX . self::SESSION_ID
+                self::PREFIX . self::USER_ID
             ))
             ->query()
             ->fetchAll(PDO::FETCH_ASSOC);
@@ -156,6 +240,9 @@ class M2E_e2M_Helper_eBay_Account {
                 case self::PREFIX . self::MODE:
                     $this->data['mode'] = (int)$row['value'];
                     continue;
+                case self::PREFIX . self::SESSION_ID:
+                    $this->data['session_id'] = $row['value'];
+                    continue;
                 case self::PREFIX . self::TOKEN:
                     $this->data['token'] = $row['value'];
                     continue;
@@ -164,9 +251,6 @@ class M2E_e2M_Helper_eBay_Account {
                     continue;
                 case self::PREFIX . self::USER_ID:
                     $this->data['user_id'] = $row['value'];
-                    continue;
-                case self::PREFIX . self::SESSION_ID:
-                    $this->data['session_id'] = $row['value'];
                     continue;
             }
         }
