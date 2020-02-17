965961742c4c491c5a78f01d9e8c5d2c1834b34a
roma_bb8 <jungle.romabb8@gmail.com>
2020-02-17 14:20:31 +0200

    c

--- app/code/local/M2E/e2M/Model/Cron/Task/Completed.php
+++ app/code/local/M2E/e2M/Model/Cron/Task/Completed.php
@@ -1,5 +1,8 @@
 <?php
 
+/**
+ * Class M2E_e2M_Model_Cron_Task_Completed
+ */
 class M2E_e2M_Model_Cron_Task_Completed implements M2E_e2M_Model_Cron_Task {
 
     /**
@@ -14,14 +17,14 @@ class M2E_e2M_Model_Cron_Task_Completed implements M2E_e2M_Model_Cron_Task {
         $connWrite = $resource->getConnection('core_write');
         $cronTasksInProcessingTableName = $resource->getTableName('m2e_e2m_cron_tasks_in_processing');
 
-        if ($progressHelper->isCompletedProgressByTag(M2E_e2M_Helper_Data::EBAY_DOWNLOAD_INVENTORY)) {
+        if ($progressHelper->isCompletedProgressByTag(M2E_e2M_Model_Cron_Task_eBay_DownloadInventory::TAG)) {
             $connWrite->delete($cronTasksInProcessingTableName, array(
                 'instance = ?' => 'Cron_Task_eBay_DownloadInventory',
                 'is_running = ?' => 0
             ));
         }
 
-        if ($progressHelper->isCompletedProgressByTag(M2E_e2M_Helper_Data::MAGENTO_IMPORT_INVENTORY)) {
+        if ($progressHelper->isCompletedProgressByTag(M2E_e2M_Model_Cron_Task_Magento_ImportInventory::TAG)) {
             $connWrite->delete($cronTasksInProcessingTableName, array(
                 'instance = ?' => 'Cron_Task_Magento_ImportInventory',
                 'is_running = ?' => 0
