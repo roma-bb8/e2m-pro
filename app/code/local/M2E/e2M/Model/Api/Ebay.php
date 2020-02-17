3a2fad19fef4906148500c915b30073284741033
roma_bb8 <jungle.romabb8@gmail.com>
2020-02-17 14:18:42 +0200

    c

--- app/code/local/M2E/e2M/Model/Api/Ebay.php
+++ app/code/local/M2E/e2M/Model/Api/Ebay.php
@@ -1,12 +1,22 @@
 <?php
 
+/**
+ * Class M2E_e2M_Model_Api_Ebay
+ */
 class M2E_e2M_Model_Api_Ebay {
 
     const MAX_PAGE_NUMBER = 50;
 
-    private $sandbox = 'https://signin.sandbox.ebay.com/ws/eBayISAPI.dll?SignIn&runame=%s&SessID=%s&ruparams=VarA%%3D%s';
-    private $url = 'https://api.sandbox.ebay.com/ws/api.dll';
+    const API_SANDBOX_ENDPOINT = 'https://api.sandbox.ebay.com/ws/api.dll';
+    const API_ENDPOINT = 'https://api.ebay.com/ws/api.dll';
+
+    const AUTH_SANDBOX_ENDPOINT = 'https://signin.sandbox.ebay.com/ws/eBayISAPI.dll?SignIn&runame=%s&SessID=%s&ruparams=VarA%%3D%s';
+    const AUTH_ENDPOINT = 'https://signin.ebay.com/ws/eBayISAPI.dll?SignIn&runame=%s&SessID=%s&ruparams=VarA%%3D%s';
+
+    /** @var string $ruName */
     private $ruName = 'xxx';
+
+    /** @var array $headers */
     private $headers = array(
         'Content-Type' => 'text/xml',
         'Expect' => ' ',
@@ -17,117 +27,45 @@ class M2E_e2M_Model_Api_Ebay {
         'X-EBAY-API-CERT-NAME' => 'xxx',
         'X-EBAY-API-CALL-NAME' => ''
     );
-    private $mode;
-
-    public function __construct() {
-
-        $access = json_decode(file_get_contents(__DIR__ . '/../../../../../../../access.json'), true);
-        $this->ruName = $access['RuName'];
-        $this->headers['X-EBAY-API-DEV-NAME'] = $access['X-EBAY-API-DEV-NAME'];
-        $this->headers['X-EBAY-API-APP-NAME'] = $access['X-EBAY-API-APP-NAME'];
-        $this->headers['X-EBAY-API-CERT-NAME'] = $access['X-EBAY-API-CERT-NAME'];
-    }
-
-    /**
-     * @return M2E_e2M_Helper_Data
-     */
-    public function getCURL() {
-        return Mage::helper('e2m');
-    }
-
-    public function setMode($mode) {
-        $this->mode = $mode;
-    }
-
 
     //########################################
 
     /**
-     * @return string
-     * @throws Exception
-     */
-    public function getSessionID() {
-
-        $this->headers['X-EBAY-API-CALL-NAME'] = 'GetSessionID';
-        $body = <<<XML
-<?xml version="1.0" encoding="utf-8" ?>
-<GetSessionIDRequest xmlns="urn:ebay:apis:eBLBaseComponents">
-    <RuName>{$this->ruName}</RuName>
-</GetSessionIDRequest>
-XML;
-
-        $cURL = $this->getCURL();
-
-        $cURL->setUrl($this->url);
-        $cURL->setHeader($this->headers);
-        $cURL->setBody($body);
-
-        $response = $cURL->getResponse();
-        $dom = new DOMDocument();
-        $dom->loadXML($response);
-
-        if (!$dom->getElementsByTagName('SessionID')->count()) {
-            throw new Exception('Not SessionID');
-        }
-
-        return $dom->getElementsByTagName('SessionID')->item(0)->nodeValue;
-    }
-
-    /**
-     * @param $backURL
-     * @param $sessionID
+     * @param int $mode
      *
      * @return string
      */
-    public function getAuthURL($backURL, $sessionID) {
-        return sprintf($this->sandbox, $this->ruName, $sessionID, $backURL);
+    private function getURL($mode) {
+
+        $url = self::API_ENDPOINT;
+        if ($mode === M2E_e2M_Helper_eBay_Account::MODE_SANDBOX) {
+            $url = self::API_SANDBOX_ENDPOINT;
+        }
+
+        return $url;
     }
 
     /**
-     * @param $sessionID
+     * @return M2E_e2M_Helper_cURL
+     */
+    private function getCURLHelper() {
+
+        /** @var M2E_e2M_Helper_cURL $cURLHelper */
+        $cURLHelper = Mage::helper('e2m/cURL');
+
+        return $cURLHelper;
+    }
+
+    //########################################
+
+    /**
+     * @param DateTime|string|int $time
      *
-     * @return array
+     * @return string
      * @throws Exception
      */
-    public function getInfo($sessionID) {
+    private function timeToString($time) {
 
-        $this->headers['X-EBAY-API-CALL-NAME'] = 'FetchToken';
-        $body = <<<XML
-<?xml version="1.0" encoding="utf-8" ?>
-<FetchTokenRequest xmlns="urn:ebay:apis:eBLBaseComponents">
-    <SessionID>{$sessionID}</SessionID>
-    <WarningLevel>High</WarningLevel>
-</FetchTokenRequest>
-XML;
-
-        $cURL = $this->getCURL();
-
-        $cURL->setUrl($this->url);
-        $cURL->setHeader($this->headers);
-        $cURL->setBody($body);
-
-        $response = $cURL->getResponse();
-        $dom = new DOMDocument();
-        $dom->loadXML($response);
-
-        if (!$dom->getElementsByTagName('eBayAuthToken')->count()) {
-            throw new Exception('Not eBayAuthToken');
-        }
-
-        $token = $dom->getElementsByTagName('eBayAuthToken')->item(0)->nodeValue;
-
-        return array(
-            'token' => $token,
-            'expiration_time' => $this->ebayTimeToString($dom->getElementsByTagName('HardExpirationTime')->item(0)->nodeValue),
-            'user_id' => $this->getGetUser($token)
-        );
-    }
-
-    public function ebayTimeToString($time) {
-        return (string)$this->getEbayDateTimeObject($time)->format('Y-m-d H:i:s');
-    }
-
-    private function getEbayDateTimeObject($time) {
         $dateTime = null;
 
         if ($time instanceof DateTime) {
@@ -142,16 +80,72 @@ XML;
             throw new Exception('ebay DateTime object is null');
         }
 
-        return $dateTime;
+        return $dateTime->format('Y-m-d H:i:s');
     }
 
+    //########################################
+
     /**
-     * @param $token
+     * @param int $mode
+     * @param string $backURL
+     * @param string $sessionID
+     *
+     * @return string
+     */
+    public function getAuthURL($mode, $backURL, $sessionID) {
+
+        $url = self::AUTH_ENDPOINT;
+        if ($mode === M2E_e2M_Helper_eBay_Account::MODE_SANDBOX) {
+            $url = self::AUTH_SANDBOX_ENDPOINT;
+        }
+
+        return sprintf($url, $this->ruName, $sessionID, $backURL);
+    }
+
+    //########################################
+
+    /**
+     * @param int $mode
      *
      * @return string
      * @throws Exception
      */
-    public function getGetUser($token) {
+    public function getSessionID($mode) {
+
+        $this->headers['X-EBAY-API-CALL-NAME'] = 'GetSessionID';
+        $body = <<<XML
+<?xml version="1.0" encoding="utf-8" ?>
+<GetSessionIDRequest xmlns="urn:ebay:apis:eBLBaseComponents">
+    <RuName>{$this->ruName}</RuName>
+</GetSessionIDRequest>
+XML;
+
+        $cURL = $this->getCURLHelper();
+        $cURL->setUrl($this->getURL($mode));
+        $cURL->setHeaders($this->headers);
+        $cURL->setBody($body);
+
+        $response = $cURL->getResponse();
+        $dom = new DOMDocument();
+        $dom->loadXML($response);
+
+        if (!$dom->getElementsByTagName('SessionID')->count()) {
+            throw new Exception('Not SessionID');
+        }
+
+        return $dom->getElementsByTagName('SessionID')->item(0)->nodeValue;
+    }
+
+    //########################################
+
+    /**
+     * @param int $mode
+     * @param string $token
+     *
+     * @return string
+     * @throws Exception
+     */
+    public function getGetUser($mode, $token) {
 
         $this->headers['X-EBAY-API-CALL-NAME'] = 'GetUser';
         $body = <<<XML
@@ -164,10 +158,9 @@ XML;
 </GetUserRequest>
 XML;
 
-        $cURL = $this->getCURL();
-
-        $cURL->setUrl($this->url);
-        $cURL->setHeader($this->headers);
+        $cURL = $this->getCURLHelper();
+        $cURL->setUrl($this->getURL($mode));
+        $cURL->setHeaders($this->headers);
         $cURL->setBody($body);
 
         $response = $cURL->getResponse();
@@ -181,21 +174,65 @@ XML;
         return $dom->getElementsByTagName('UserID')->item(0)->nodeValue;
     }
 
+    //########################################
+
     /**
+     * @param int $mode
+     * @param string $sessionID
+     *
+     * @return array
+     * @throws Exception
+     */
+    public function getInfo($mode, $sessionID) {
+
+        $this->headers['X-EBAY-API-CALL-NAME'] = 'FetchToken';
+        $body = <<<XML
+<?xml version="1.0" encoding="utf-8" ?>
+<FetchTokenRequest xmlns="urn:ebay:apis:eBLBaseComponents">
+    <SessionID>{$sessionID}</SessionID>
+    <WarningLevel>High</WarningLevel>
+</FetchTokenRequest>
+XML;
+
+        $cURL = $this->getCURLHelper();
+        $cURL->setUrl($this->getURL($mode));
+        $cURL->setHeaders($this->headers);
+        $cURL->setBody($body);
+
+        $response = $cURL->getResponse();
+        $dom = new DOMDocument();
+        $dom->loadXML($response);
+
+        if (!$dom->getElementsByTagName('eBayAuthToken')->count()) {
+            throw new Exception('Not eBayAuthToken');
+        }
+
+        $token = $dom->getElementsByTagName('eBayAuthToken')->item(0)->nodeValue;
+        return array(
+            'token' => $token,
+            'expiration_time' => $this->timeToString($dom->getElementsByTagName('HardExpirationTime')->item(0)->nodeValue),
+            'user_id' => $this->getGetUser($mode, $token)
+        );
+    }
+
+    //########################################
+
+    /**
+     * @param int $mode
      * @param string $token
      * @param DateTime $fromDateTime
      * @param DateTime $toDateTime
      *
      * @throws Exception
      */
-    public function downloadInventory($token, $fromDateTime, $toDateTime) {
+    public function downloadInventory($mode, $token, $fromDateTime, $toDateTime) {
 
         $this->headers['X-EBAY-API-CALL-NAME'] = 'GetSellerList';
 
         //----------------------------------------
 
-        /** @var M2e_e2m_Helper_Full $parser */
-        $itemParserHelper = Mage::helper('e2m/Full');
+        /** @var M2E_e2M_Model_Parser_eBay_Item $parser */
+        $parser = Mage::getModel('e2m/Parser_eBay_Item');
         $coreHelper = Mage::helper('core');
 
         $resource = Mage::getSingleton('core/resource');
@@ -205,6 +242,10 @@ XML;
         $inventoryTableName = $resource->getTableName('m2e_e2m_inventory_ebay');
 
         //----------------------------------------
+
+        $cURL = $this->getCURLHelper();
+        $cURL->setUrl($this->getURL($mode));
+        $cURL->setHeaders($this->headers);
 
         $pageNumber = 1;
         while (self::MAX_PAGE_NUMBER > $pageNumber) {
@@ -225,11 +266,6 @@ XML;
     </Pagination>
 </GetSellerListRequest>
 XML;
-
-            $cURL = $this->getCURL();
-
-            $cURL->setUrl($this->url);
-            $cURL->setHeader($this->headers);
             $cURL->setBody($body);
 
             $response = new SimpleXMLElement($cURL->getResponse());
@@ -239,7 +275,7 @@ XML;
 
             $items = array();
             foreach ($response->ItemArray->Item as $xmlItem) {
-                $item = $itemParserHelper->parseItem($xmlItem);
+                $item = $parser->parseItem($xmlItem);
                 $items[$item['identifiers']['item_id']] = $item;
             }
 
@@ -282,4 +318,14 @@ XML;
     }
 
     //########################################
+
+    /** @tmp */
+    public function __construct() {
+
+        $access = json_decode(file_get_contents(__DIR__ . '/../../../../../../../access.json'), true);
+        $this->ruName = $access['RuName'];
+        $this->headers['X-EBAY-API-DEV-NAME'] = $access['X-EBAY-API-DEV-NAME'];
+        $this->headers['X-EBAY-API-APP-NAME'] = $access['X-EBAY-API-APP-NAME'];
+        $this->headers['X-EBAY-API-CERT-NAME'] = $access['X-EBAY-API-CERT-NAME'];
+    }
 }
