a6b0f463f9a3f4e669989a8a2f07a4d034bdf0e3
roma_bb8 <jungle.romabb8@gmail.com>
2020-02-17 13:35:41 +0200

    c

--- app/code/local/M2E/e2M/Helper/eBay/Inventory.php
+++ app/code/local/M2E/e2M/Helper/eBay/Inventory.php
@@ -1,5 +1,8 @@
 <?php
 
+/**
+ * Class M2E_e2M_Helper_eBay_Inventory
+ */
 class M2E_e2M_Helper_eBay_Inventory {
 
     const PREFIX = M2E_e2M_Helper_Data::PREFIX . 'inventory/';
@@ -9,7 +12,98 @@ class M2E_e2M_Helper_eBay_Inventory {
     const PATH_ITEMS_COUNT_SIMPLE = 'items/count/simple';
     const PATH_MARKETPLACES = 'marketplaces';
 
+    const MARKETPLACE_CUSTOM_CODE_TITLE = 'Custom';
+    const MARKETPLACE_AU_TITLE = 'Australia';
+    const MARKETPLACE_AT_TITLE = 'Austria';
+    const MARKETPLACE_BE_DU_TITLE = 'Belgium Dutch';
+    const MARKETPLACE_BE_FR_TITLE = 'Belgium French';
+    const MARKETPLACE_CA_TITLE = 'Canada';
+    const MARKETPLACE_CA_FR_TITLE = 'Canada French';
+    const MARKETPLACE_MOTORS_TITLE = 'eBay Motors';
+    const MARKETPLACE_FR_TITLE = 'France';
+    const MARKETPLACE_DE_TITLE = 'Germany';
+    const MARKETPLACE_HK_TITLE = 'Hong Kong';
+    const MARKETPLACE_IN_TITLE = 'India';
+    const MARKETPLACE_IE_TITLE = 'Ireland';
+    const MARKETPLACE_IT_TITLE = 'Italy';
+    const MARKETPLACE_MY_TITLE = 'Malaysia';
+    const MARKETPLACE_NL_TITLE = 'Netherlands';
+    const MARKETPLACE_PH_TITLE = 'Philippines';
+    const MARKETPLACE_PL_TITLE = 'Poland';
+    const MARKETPLACE_RU_TITLE = 'Russia';
+    const MARKETPLACE_SG_TITLE = 'Singapore';
+    const MARKETPLACE_SP_TITLE = 'Spain';
+    const MARKETPLACE_CH_TITLE = 'Switzerland';
+    const MARKETPLACE_UK_TITLE = 'United Kingdom';
+    const MARKETPLACE_US_TITLE = 'United States';
+
     //########################################
+
+    private $marketplaceTitles = array(
+        M2E_e2M_Helper_Item::MARKETPLACE_CUSTOM_CODE_ID => self::MARKETPLACE_CUSTOM_CODE_TITLE,
+        M2E_e2M_Helper_Item::MARKETPLACE_AU_ID => self::MARKETPLACE_AU_TITLE,
+        M2E_e2M_Helper_Item::MARKETPLACE_AT_ID => self::MARKETPLACE_AT_TITLE,
+        M2E_e2M_Helper_Item::MARKETPLACE_BE_DU_ID => self::MARKETPLACE_BE_DU_TITLE,
+        M2E_e2M_Helper_Item::MARKETPLACE_BE_FR_ID => self::MARKETPLACE_BE_FR_TITLE,
+        M2E_e2M_Helper_Item::MARKETPLACE_CA_ID => self::MARKETPLACE_CA_TITLE,
+        M2E_e2M_Helper_Item::MARKETPLACE_CA_FR_ID => self::MARKETPLACE_CA_FR_TITLE,
+        M2E_e2M_Helper_Item::MARKETPLACE_MOTORS_ID => self::MARKETPLACE_MOTORS_TITLE,
+        M2E_e2M_Helper_Item::MARKETPLACE_FR_ID => self::MARKETPLACE_FR_TITLE,
+        M2E_e2M_Helper_Item::MARKETPLACE_DE_ID => self::MARKETPLACE_DE_TITLE,
+        M2E_e2M_Helper_Item::MARKETPLACE_HK_ID => self::MARKETPLACE_HK_TITLE,
+        M2E_e2M_Helper_Item::MARKETPLACE_IN_ID => self::MARKETPLACE_IN_TITLE,
+        M2E_e2M_Helper_Item::MARKETPLACE_IE_ID => self::MARKETPLACE_IE_TITLE,
+        M2E_e2M_Helper_Item::MARKETPLACE_IT_ID => self::MARKETPLACE_IT_TITLE,
+        M2E_e2M_Helper_Item::MARKETPLACE_MY_ID => self::MARKETPLACE_MY_TITLE,
+        M2E_e2M_Helper_Item::MARKETPLACE_NL_ID => self::MARKETPLACE_NL_TITLE,
+        M2E_e2M_Helper_Item::MARKETPLACE_PH_ID => self::MARKETPLACE_PH_TITLE,
+        M2E_e2M_Helper_Item::MARKETPLACE_PL_ID => self::MARKETPLACE_PL_TITLE,
+        M2E_e2M_Helper_Item::MARKETPLACE_RU_ID => self::MARKETPLACE_RU_TITLE,
+        M2E_e2M_Helper_Item::MARKETPLACE_SG_ID => self::MARKETPLACE_SG_TITLE,
+        M2E_e2M_Helper_Item::MARKETPLACE_SP_ID => self::MARKETPLACE_SP_TITLE,
+        M2E_e2M_Helper_Item::MARKETPLACE_CH_ID => self::MARKETPLACE_CH_TITLE,
+        M2E_e2M_Helper_Item::MARKETPLACE_UK_ID => self::MARKETPLACE_UK_TITLE,
+        M2E_e2M_Helper_Item::MARKETPLACE_US_ID => self::MARKETPLACE_US_TITLE
+    );
+
+    private $eBayFields = array(
+        'identifiers][item_id' => 'Item ID',
+        'identifiers][sku' => 'SKU',
+        'identifiers][ean' => 'EAN',
+        'identifiers][upc' => 'UPC',
+        'identifiers][isbn' => 'ISBN',
+        'identifiers][epid' => 'EPID',
+        'identifiers][brand_mpn][mpn' => '(Brand) MPN',
+        'identifiers][brand_mpn][brand' => '(Brand) Brand',
+
+        'marketplace_id' => '(Site) Marketplace ID',
+        'categories][primary][id' => '(Category) Primary ID',
+        'categories][secondary][id' => '(Category) Secondary ID',
+        'store][categories][primary][id' => '(Store) Category ID',
+        'store][categories][secondary][id' => '(Store) Category 2 ID',
+
+        'description][title' => 'Title',
+        'description][subtitle' => 'SubTitle',
+        'description][description' => 'Description',
+
+        'price][start' => 'Start Price',
+        'price][current' => 'Current Price',
+        'price][buy_it_now' => 'Buy It Now Price',
+        'price][original' => 'Original Price',
+        'price][map][value' => '(DPI) Minimum Advertised Price',
+        'price][map][exposure' => '(DPI) Minimum Advertised Price Exposure',
+        'price][stp][value' => '(Discount Price Info) Original Retail Price',
+
+        'qty][total' => 'Quantity',
+
+        'shipping][dispatch_time' => 'Dispatch Time',
+        'shipping][package][dimensions][depth' => '(Dimensions) Depth',
+        'shipping][package][dimensions][length' => '(Dimensions) Length',
+        'shipping][package][dimensions][width' => '(Dimensions) Width',
+        'shipping][package][dimensions][unit_type' => 'Unit Type',
+
+        'condition][type' => 'Condition ID'
+    );
 
     /** @var Mage_Core_Model_Resource $resource */
     private $resource;
@@ -23,31 +117,46 @@ class M2E_e2M_Helper_eBay_Inventory {
     /** @var Mage_Core_Helper_Data */
     private $coreHelper;
 
-    //########################################
-
+    /** @var array $inventory */
     private $inventory = array();
 
     //########################################
 
     /**
+     * @return array
+     */
+    public function getEbayFields() {
+        return $this->eBayFields;
+    }
+
+    /**
+     * @param int $marketplaceId
+     *
+     * @return string
+     */
+    public function getMarketplaceTitle($marketplaceId) {
+        return $this->marketplaceTitles[$marketplaceId];
+    }
+
+    /**
      * @return int
      */
     public function getItemsTotal() {
-        return (int)$this->inventory['items']['count']['total'];
+        return $this->inventory['items']['count']['total'];
     }
 
     /**
      * @return int
      */
     public function getItemsVariation() {
-        return (int)$this->inventory['items']['count']['variation'];
+        return $this->inventory['items']['count']['variation'];
     }
 
     /**
      * @return int
      */
     public function getItemsSimple() {
-        return (int)$this->inventory['items']['count']['simple'];
+        return $this->inventory['items']['count']['simple'];
     }
 
     /**
@@ -63,15 +172,15 @@ class M2E_e2M_Helper_eBay_Inventory {
 
         $connRead = $this->resource->getConnection('core_read');
 
-        $this->inventory['items']['count']['total'] = $connRead->select()
+        $this->inventory['items']['count']['total'] = (int)$connRead->select()
             ->from($this->inventoryTableName, 'COUNT(*)')
             ->query()->fetchColumn();
 
-        $this->inventory['items']['count']['variation'] = $connRead->select()
+        $this->inventory['items']['count']['variation'] = (int)$connRead->select()
             ->from($this->inventoryTableName, 'COUNT(*)')
             ->where('variation = ?', true)->query()->fetchColumn();
 
-        $this->inventory['items']['count']['simple'] = $connRead->select()
+        $this->inventory['items']['count']['simple'] = (int)$connRead->select()
             ->from($this->inventoryTableName, 'COUNT(*)')
             ->where('variation = ?', false)->query()->fetchColumn();
 
@@ -84,6 +193,9 @@ class M2E_e2M_Helper_eBay_Inventory {
 
     //########################################
 
+    /**
+     * @return $this
+     */
     public function save() {
 
         $connWrite = $this->resource->getConnection('core_write');
@@ -117,6 +229,9 @@ class M2E_e2M_Helper_eBay_Inventory {
         return $this;
     }
 
+    /**
+     * M2E_e2M_Helper_eBay_Inventory constructor.
+     */
     public function __construct() {
 
         $this->coreHelper = Mage::helper('core');
