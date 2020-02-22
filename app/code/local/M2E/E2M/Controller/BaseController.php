6039377dd7b3820f0846a2f15fc9518bf8389e68
roma_bb8 <jungle.romabb8@gmail.com>
2020-02-22 23:28:15 +0200

    ref

---
+++ app/code/local/M2E/E2M/Controller/BaseController.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * @author     M2E Pro Developers Team
+ * @copyright  M2E LTD
+ * @license    Commercial use is forbidden
+ */
+
+/**
+ * Class Ess_E2M_Controller_Adminhtml_BaseController
+ */
+class Ess_E2M_Controller_Adminhtml_BaseController extends Mage_Adminhtml_Controller_Action {
+
+    const HTTP_INTERNAL_ERROR = 500;
+
+    //########################################
+
+    /**
+     * @param Exception $e
+     *
+     * @return Zend_Controller_Response_Abstract
+     * @throws Zend_Controller_Response_Exception
+     */
+    public function renderAjaxException(Exception $e) {
+
+        /** @var M2E_E2M_Helper_Data $dataHelper */
+        $dataHelper = Mage::helper('e2m');
+        $dataHelper->logException($e);
+
+        $response = $this->getResponse();
+        $response->setHttpResponseCode(self::HTTP_INTERNAL_ERROR);
+        $response->setBody(Mage::helper('core')->jsonEncode(array(
+            'error' => true,
+            'message' => $e->getMessage()
+        )));
+
+        return $response;
+    }
+
+    //########################################
+
+    /**
+     * @inheritDoc
+     * @throws Zend_Controller_Response_Exception
+     */
+    final public function dispatch($action) {
+
+        try {
+            parent::dispatch($action);
+        } catch (Exception $e) {
+            if ($this->getRequest()->isAjax()) {
+                return $this->renderAjaxException($e);
+            }
+
+            $this->_getSession()->addError($e->getMessage());
+        }
+
+        return $this->_redirect('*/e2m/index');
+    }
+}
