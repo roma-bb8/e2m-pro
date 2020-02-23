ca875804a6a0b7d36983906e3b9f3a0432b6c821
roma_bb8 <jungle.romabb8@gmail.com>
2020-02-23 23:25:45 +0200

    ref

--- app/code/local/M2E/E2M/Model/Cron/Task/Magento/ImportInventory.php
+++ app/code/local/M2E/E2M/Model/Cron/Task/Magento/ImportInventory.php
@@ -42,38 +42,44 @@ class M2E_E2M_Model_Cron_Task_Magento_ImportInventory implements M2E_E2M_Model_C
      */
     public function process($taskId, $data) {
 
+        /** @var M2E_E2M_Model_Product_Magento_Configurable $productMagentoConfigurable */
+        $productMagentoConfigurable = Mage::getModel('e2m/Product_Magento_Configurable');
+
+        /** @var M2E_E2M_Model_Product_Magento_Simple $productMagentoSimple */
+        $productMagentoSimple = Mage::getModel('e2m/Product_Magento_Simple');
+
+        /** @var M2E_E2M_Model_Ebay_Config $eBayConfig */
+        $eBayConfig = Mage::getModel('e2m/Ebay_Config');
+
+        /** @var M2E_E2M_Helper_Data $dataHelper */
+        $dataHelper = Mage::helper('e2m');
+
+        /** @var M2E_E2M_Helper_Progress $progressHelper */
+        $progressHelper = Mage::helper('e2m/Progress');
+
         $coreHelper = Mage::helper('core');
+
         $resource = Mage::getSingleton('core/resource');
 
-        $connRead = $resource->getConnection('core_read');
         $connWrite = $resource->getConnection('core_write');
+        $connRead = $resource->getConnection('core_read');
+
         $inventoryTableName = $resource->getTableName('m2e_e2m_inventory_ebay');
         $cronTasksInProcessingTableName = $resource->getTableName('m2e_e2m_cron_tasks_in_processing');
 
         //----------------------------------------
 
-        /** @var M2E_E2M_Model_Product_Magento_Configurable $productMagentoConfigurable */
-        $productMagentoConfigurable = Mage::getModel('e2m/Product_Magento_Configurable');
         $productMagentoConfigurable->setTaskId($taskId);
-
-        /** @var M2E_E2M_Model_Product_Magento_Simple $productMagentoSimple */
-        $productMagentoSimple = Mage::getModel('e2m/Product_Magento_Simple');
         $productMagentoSimple->setTaskId($taskId);
 
-        $query = $connRead->select()
-            ->from($inventoryTableName)
-            ->where('id > ?', $data['last_import_id'])
-            ->limit(self::MAX_LIMIT)
-            ->query();
+        //----------------------------------------
 
-        /** @var M2E_E2M_Helper_Data $dataHelper */
-        $dataHelper = Mage::helper('e2m');
-
-        /** @var M2E_E2M_Helper_eBay_Config $eBayConfigHelper */
-        $eBayConfigHelper = Mage::helper('e2m/eBay_Config');
+        $query = $connRead->select()->from($inventoryTableName)
+            ->where('id > ?', $data['last_import_id'])->limit(self::MAX_LIMIT)->query();
         while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
+
             $rowData = $coreHelper->jsonDecode($row['data']);
-            if ($eBayConfigHelper->isSkipStore($rowData['marketplace_id'])) {
+            if ($eBayConfig->isSkipStore($rowData['marketplace_id'])) {
                 $dataHelper->logReport(
                     $row['id'], 'Skip marketplace eBay item: ' . $rowData['identifiers_item_id'],
                     M2E_E2M_Helper_Data::TYPE_REPORT_WARNING
@@ -86,15 +92,16 @@ class M2E_E2M_Model_Cron_Task_Magento_ImportInventory implements M2E_E2M_Model_C
                 : $productMagentoSimple->process($rowData);
 
             $data['last_import_id'] = $row['id'];
+
             $connWrite->update($cronTasksInProcessingTableName, array(
                 'data' => Mage::helper('core')->jsonEncode($data)
             ), array('instance = ?' => 'Cron_Task_Magento_ImportInventory'));
         }
 
-        /** @var M2E_E2M_Helper_Progress $progressHelper */
-        $progressHelper = Mage::helper('e2m/Progress');
+        //----------------------------------------
 
         $process = $this->getProcessAsPercentage($data['last_import_id']);
+
         $progressHelper->setProgressByTag(self::TAG, $process);
 
         //----------------------------------------
