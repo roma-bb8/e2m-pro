5ad684f7b7b5fdcb149759184372510e4f07b551
roma_bb8 <jungle.romabb8@gmail.com>
2020-02-23 23:33:22 +0200

    ref

--- app/code/local/M2E/E2M/Model/Cron/Task/Completed.php
+++ app/code/local/M2E/E2M/Model/Cron/Task/Completed.php
@@ -15,38 +15,47 @@ class M2E_E2M_Model_Cron_Task_Completed implements M2E_E2M_Model_Cron_Task {
      */
     public function process($taskId, $data) {
 
+        /** @var M2E_E2M_Helper_Data $dataHelper */
+        $dataHelper = Mage::helper('e2m');
+
         /** @var M2E_E2M_Helper_Progress $progressHelper */
         $progressHelper = Mage::helper('e2m/Progress');
 
         $resource = Mage::getSingleton('core/resource');
+
         $connWrite = $resource->getConnection('core_write');
+        $connRead = $resource->getConnection('core_read');
+
         $cronTasksInProcessingTableName = $resource->getTableName('m2e_e2m_cron_tasks_in_processing');
 
-        if ($progressHelper->isCompletedProgressByTag(M2E_E2M_Model_Cron_Task_eBay_DownloadInventory::TAG)) {
-            $id = $connWrite->delete($cronTasksInProcessingTableName, array(
-                'instance = ?' => 'Cron_Task_eBay_DownloadInventory',
-                'is_running = ?' => 0
-            ));
+        //----------------------------------------
 
-            if ($id >= 1) {
-                Mage::helper('e2m')->logReport($id, 'Finish task of Downloading Inventory from eBay.',
-                    M2E_E2M_Helper_Data::TYPE_REPORT_SUCCESS
-                );
+        if ($progressHelper->isCompletedProgressByTag(M2E_E2M_Model_Cron_Task_Ebay_DownloadInventory::TAG)) {
+
+            $taskId = $connRead->select()->from($cronTasksInProcessingTableName, array('id'))
+                ->where('instance = ?', 'Cron_Task_Ebay_DownloadInventory')->query()->fetchColumn();
+
+            if ($taskId) {
+                $connWrite->delete($cronTasksInProcessingTableName, array(
+                    'id = ?' => $taskId
+                ));
+
+                $dataHelper->logReport($taskId, 'Finish task of Downloading Inventory from eBay.');
             }
         }
 
         if ($progressHelper->isCompletedProgressByTag(M2E_E2M_Model_Cron_Task_Magento_ImportInventory::TAG)) {
-            $id = $connWrite->delete($cronTasksInProcessingTableName, array(
-                'instance = ?' => 'Cron_Task_Magento_ImportInventory',
-                'is_running = ?' => 0
-            ));
-            if ($id >= 1) {
 
-                Mage::helper('e2m')->logReport($id, 'Finish task of Import Inventory from Magento.',
-                    M2E_E2M_Helper_Data::TYPE_REPORT_SUCCESS
-                );
+            $taskId = $connRead->select()->from($cronTasksInProcessingTableName, array('id'))
+                ->where('instance = ?', 'Cron_Task_Magento_ImportInventory')->query()->fetchColumn();
+
+            if ($taskId) {
+                $connWrite->delete($cronTasksInProcessingTableName, array(
+                    'id = ?' => $taskId
+                ));
+
+                $dataHelper->logReport($taskId, 'Finish task of Import Inventory from Magento.');
             }
-
         }
 
         return array();
