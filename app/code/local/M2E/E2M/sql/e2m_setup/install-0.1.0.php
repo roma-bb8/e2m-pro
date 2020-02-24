f5de79db4ca31474e1764ef08b2659c74e3c7add
roma_bb8 <jungle.romabb8@gmail.com>
2020-02-24 11:30:04 +0200

    ref

--- app/code/local/M2E/E2M/sql/e2m_setup/install-0.1.0.php
+++ app/code/local/M2E/E2M/sql/e2m_setup/install-0.1.0.php
@@ -18,6 +18,7 @@ CREATE TABLE `{$installer->getTable('m2e_e2m_cron_tasks_in_processing')}` (
   `pause` BOOL NOT NULL DEFAULT FALSE,
   `instance` VARCHAR(255) NOT NULL,
   `data` TEXT DEFAULT NULL,
+  `progress` TINYINT DEFAULT 0,
   `updated` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `created` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
@@ -49,8 +50,33 @@ CREATE TABLE `{$installer->getTable('m2e_e2m_log')}` (
   PRIMARY KEY (`id`)
 ) ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;
 
-INSERT INTO `{$installer->getTable('m2e_e2m_cron_tasks_in_processing')}` (`instance`, `data`)
-VALUES ('Cron_Task_Completed', '{}');
+INSERT INTO `{$installer->getTable('m2e_e2m_cron_tasks_in_processing')}`
+    (`instance`, `data`)
+VALUES
+    ('Cron_Task_Completed', '{}');
+
+INSERT INTO `{$installer->getTable('core_config_data')}`
+    (`path`, `value`)
+VALUES
+    ('/m2e/e2m/ebay/account/mode/', NULL),
+    ('/m2e/e2m/ebay/account/session_id/', NULL),
+    ('/m2e/e2m/ebay/account/token/', NULL),
+    ('/m2e/e2m/ebay/account/expiration_time/', NULL),
+    ('/m2e/e2m/ebay/account/user_id/', NULL),
+    ('/m2e/e2m/settings/marketplaces/stores/map/', NULL),
+    ('/m2e/e2m/settings/inventory/product_identifier/', NULL),
+    ('/m2e/e2m/settings/inventory/action_found/', NULL),
+    ('/m2e/e2m/settings/product/import/qty/', NULL),
+    ('/m2e/e2m/settings/product/generate_sku/', NULL),
+    ('/m2e/e2m/settings/product/import/image/', NULL),
+    ('/m2e/e2m/settings/product/delete_html/', NULL),
+    ('/m2e/e2m/settings/product/attribute_set/', NULL),
+    ('/m2e/e2m/settings/product/fields_attributes/map/', NULL),
+    ('/m2e/e2m/settings/full/', NULL),
+    ('/m2e/e2m/inventory/items/count/total/', NULL),
+    ('/m2e/e2m/inventory/items/count/variation/', NULL),
+    ('/m2e/e2m/inventory/items/count/simple/', NULL),
+    ('/m2e/e2m/inventory/marketplaces/', NULL);
 SQL;
 
 $installer->run($sql);
