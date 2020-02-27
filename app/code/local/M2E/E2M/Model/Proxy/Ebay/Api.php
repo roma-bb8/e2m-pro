e45ae9b6e9bf3f7ab438340e0ba1173ddd5cc932
roma_bb8 <jungle.romabb8@gmail.com>
2020-02-27 02:29:29 +0200

    x

---
+++ app/code/local/M2E/E2M/Model/Proxy/Ebay/Api.php
@@ -0,0 +1,37 @@
+<?php
+
+class M2E_E2M_Model_Proxy_Ebay_Api {
+
+    /** @var Ess_M2ePro_Model_Ebay_Connector_Protocol $eBayConnectorProtocol */
+    private $eBayConnectorProtocol;
+
+    /** @var Ess_M2ePro_Model_Connector_Connection_Request $connectorConnectionRequest */
+    private $connectorConnectionRequest;
+
+    /** @var Ess_M2ePro_Model_Connector_Connection_Single $connectorConnectionSingle */
+    private $connectorConnectionSingle;
+
+    //########################################
+
+    public function sendRequest(array $request) {
+
+        $this->connectorConnectionRequest->setComponentVersion($this->eBayConnectorProtocol->getComponentVersion());
+        $this->connectorConnectionRequest->setComponent($this->eBayConnectorProtocol->getComponent());
+        $this->connectorConnectionRequest->setCommand((array)$request['command']);
+        $this->connectorConnectionRequest->setData($request['data']);
+
+        $this->connectorConnectionSingle->setRequest($this->connectorConnectionRequest);
+        $this->connectorConnectionSingle->process();
+
+        return $this->connectorConnectionSingle->getResponse()->getData();
+    }
+
+    //########################################
+
+    public function __construct() {
+
+        $this->eBayConnectorProtocol = Mage::getModel('M2ePro/Ebay_Connector_Protocol');
+        $this->connectorConnectionSingle = Mage::getModel('M2ePro/Connector_Connection_Single');
+        $this->connectorConnectionRequest = Mage::getModel('M2ePro/Connector_Connection_Request');
+    }
+}
