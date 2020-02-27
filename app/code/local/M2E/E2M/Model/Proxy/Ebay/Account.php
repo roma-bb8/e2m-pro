1a5e1f6700992f573c4c4e3c2d427e9b9098d748
roma_bb8 <jungle.romabb8@gmail.com>
2020-02-27 02:21:13 +0200

    x

---
+++ app/code/local/M2E/E2M/Model/Proxy/Ebay/Account.php
@@ -0,0 +1,46 @@
+<?php
+
+class M2E_E2M_Model_Proxy_Ebay_Account {
+
+    const XML_PATH_EBAY_ACCOUNT_ID = M2E_E2M_Helper_Data::PREFIX . 'ebay/account/id';
+
+    //########################################
+
+    /** @var Ess_M2ePro_Model_Ebay_Account $eBayAccount */
+    private $eBayAccount;
+
+    //########################################
+
+    public function getToken() {
+        return $this->eBayAccount->getServerHash();
+    }
+
+    public function isSandboxMode() {
+        return $this->eBayAccount->isModeSandbox();
+    }
+
+    public function getUserId() {
+        return $this->eBayAccount->getUserId();
+    }
+
+    public function getAccountUrl() {
+
+        $domain = 'ebay.com';
+        $this->isSandboxMode() && $domain = 'sandbox.' . $domain;
+
+        return sprintf('https://%s/%s', $domain, $this->getUserId());
+    }
+
+    //########################################
+
+    public function __construct() {
+
+        $this->eBayAccount = Mage::getModel('M2ePro/Ebay_Account');
+        $dataHelper = Mage::helper('e2m');
+
+        $id = $dataHelper->getConfig(self::XML_PATH_EBAY_ACCOUNT_ID);
+        if (!empty($id)) {
+            $this->eBayAccount->load($id);
+        }
+    }
+}
