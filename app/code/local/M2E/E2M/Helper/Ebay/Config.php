8ae4671b2bdedfc82092674d5e2c2913f9dc8cb9
roma_bb8 <jungle.romabb8@gmail.com>
2020-04-26 01:18:39 +0300

    v3 ref

--- app/code/local/M2E/E2M/Helper/Ebay/Config.php
+++ app/code/local/M2E/E2M/Helper/Ebay/Config.php
@@ -28,21 +28,63 @@ class M2E_E2M_Helper_Ebay_Config {
      * @return string
      */
     public function getProductIdentifier() {
-        return Mage::helper('e2m')->getConfig(self::XML_PATH_PRODUCT_SKU);
+        return Mage::getModel('e2m/Config')->get(self::XML_PATH_PRODUCT_SKU);
     }
 
     /**
      * @return bool
      */
     public function isGenerateSku() {
-        return (bool)Mage::helper('e2m')->getConfig(self::XML_PATH_PRODUCT_SKU_GENERATE);
+        return (bool)Mage::getModel('e2m/Config')->get(self::XML_PATH_PRODUCT_SKU_GENERATE);
     }
 
     /**
      * @return bool
      */
     public function isDeleteHtml() {
-        return (bool)Mage::helper('e2m')->getConfig(self::XML_PATH_PRODUCT_HTML_DELETE);
+        return (bool)Mage::getModel('e2m/Config')->get(self::XML_PATH_PRODUCT_HTML_DELETE);
+    }
+
+    //########################################
+
+    /**
+     * @param string $marketplaceCode
+     *
+     * @return bool|int
+     */
+    public function getStoreIdByMarketplaceCode($marketplaceCode) {
+
+        $map = Mage::getModel('e2m/Config')->get(self::XML_PATH_STORE_MAP, array());
+
+        $marketplaceCode = strtoupper($marketplaceCode);
+        if (isset($map[$marketplaceCode])) {
+            return (int)$map[$marketplaceCode];
+        }
+
+        return false;
+    }
+
+    /**
+     * @return bool|string
+     */
+    public function getMarketplaceCodeUseAdminStore() {
+
+        $map = Mage::getModel('e2m/Config')->get(self::XML_PATH_STORE_MAP, array());
+        $site = array_search(M2E_E2M_Helper_Magento::STORE_ADMIN, $map);
+        if (!empty($site)) {
+            return strtolower($site);
+        }
+
+        return false;
+    }
+
+    /**
+     * @param string $marketplaceCode
+     *
+     * @return bool
+     */
+    public function isMarketplaceSkip($marketplaceCode) {
+        return M2E_E2M_Helper_Magento::STORE_SKIP === $this->getStoreIdByMarketplaceCode($marketplaceCode);
     }
 
     //########################################
@@ -54,19 +96,18 @@ class M2E_E2M_Helper_Ebay_Config {
      */
     public function applySettings(array $item) {
 
-        if (Mage::helper('e2m/Ebay_Config')->isDeleteHtml()) {
+        if ($this->isDeleteHtml()) {
             $item['ebay_title'] = strip_tags($item['ebay_title']);
             $item['ebay_subtitle'] = strip_tags($item['ebay_subtitle']);
             $item['ebay_description'] = strip_tags($item['ebay_description']);
         }
 
-        $productSKU = Mage::helper('e2m/Ebay_Config')->getProductIdentifier();
-        if (empty($item[$productSKU]) && Mage::helper('e2m/Ebay_Config')->isGenerateSku()) {
+        $productSKU = $this->getProductIdentifier();
+        if (empty($item[$productSKU]) && $this->isGenerateSku()) {
             $item[$productSKU] = 'SKU_' . md5($item['ebay_item_id'] . $item['item_variation_id']);
         }
 
-        if (self::DOES_NOT_APPLY === strtolower($item[$productSKU]) &&
-            Mage::helper('e2m/Ebay_Config')->isGenerateSku()) {
+        if (self::DOES_NOT_APPLY === strtolower($item[$productSKU]) && $this->isGenerateSku()) {
             $item[$productSKU] = 'DNA_' . md5($item['ebay_item_id'] . $item['item_variation_id']);
         }
 
