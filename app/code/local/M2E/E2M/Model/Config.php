307b28ad74e6d0a68678426625cbeffa1f56482d
roma_bb8 <jungle.romabb8@gmail.com>
2020-04-25 21:58:39 +0300

    v3 ref

---
+++ app/code/local/M2E/E2M/Model/Config.php
@@ -0,0 +1,48 @@
+<?php
+
+
+class M2E_E2M_Model_Config {
+
+    /**
+     * @param string $path
+     * @param mixed $value
+     * @param bool $cleanCache
+     *
+     * @return $this
+     */
+    public function set($path, $value, $cleanCache = false) {
+
+        Mage::getModel('core/config')->saveConfig(
+            $path,
+            Mage::helper('core')->jsonEncode($value)
+        );
+
+        $cleanCache && Mage::getModel('core/config')->cleanCache();
+
+        return $this;
+    }
+
+    /**
+     * @param string $path
+     * @param mixed|null $default
+     *
+     * @return mixed|null
+     */
+    public function get($path, $default = null) {
+
+        try {
+
+            $value = Mage::app()->getStore()->getConfig($path);
+            if (!$value) {
+                return $default;
+            }
+
+        } catch (Mage_Core_Model_Store_Exception $e) {
+            Mage::helper('e2m')->writeExceptionLog($e);
+
+            return $default;
+        }
+
+        return Mage::helper('core')->jsonDecode($value);
+    }
+}
