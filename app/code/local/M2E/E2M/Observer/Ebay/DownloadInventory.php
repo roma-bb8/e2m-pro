42b06786baa1b58744c028aeade4d6200db71211
roma_bb8 <jungle.romabb8@gmail.com>
2020-04-26 05:22:08 +0300

    v3 ref

--- app/code/local/M2E/E2M/Observer/Ebay/DownloadInventory.php
+++ app/code/local/M2E/E2M/Observer/Ebay/DownloadInventory.php
@@ -4,27 +4,20 @@ class M2E_E2M_Observer_Ebay_DownloadInventory {
 
     public function process() {
 
-        /** @var M2E_E2M_Helper_Data $dataHelper */
-        $dataHelper = Mage::helper('e2m');
-
         $resource = Mage::getSingleton('core/resource');
-
-        $connRead = $resource->getConnection('core_read');
-
-        $inventoryEbayTableName = $resource->getTableName('m2e_e2m_ebay_items');
-
-        //----------------------------------------
+        $sites = $resource->getConnection('core_read')->select()
+            ->from($resource->getTableName('m2e_e2m_ebay_items'), 'site')
+            ->group('site')->query()->fetchAll(PDO::FETCH_ASSOC);
 
         $marketplaces = array();
-        foreach ($connRead->select()->from($inventoryEbayTableName, 'site')
-                     ->group('site')->distinct()->query()->fetchAll(PDO::FETCH_ASSOC) as $row) {
-            $marketplaces[] = $row['site'];
+        foreach ($sites as $item) {
+            $marketplaces[] = $item['site'];
         }
 
-        //----------------------------------------
-
-        $dataHelper->setConfig(M2E_E2M_Helper_Data::XML_PATH_EBAY_AVAILABLE_MARKETPLACES, $marketplaces, true);
-
-        Mage::dispatchEvent('m2e_e2m_available_marketplaces');
+        Mage::helper('e2m/Config')->set(
+            M2E_E2M_Helper_Ebay::XML_PATH_AVAILABLE_MARKETPLACES,
+            $marketplaces,
+            true
+        );
     }
 }
