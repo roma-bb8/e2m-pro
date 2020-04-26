71fda27c25fd6f51625113eaad374c3df8bdf44e
roma_bb8 <jungle.romabb8@gmail.com>
2020-04-26 04:53:47 +0300

    v3 ref

--- app/code/local/M2E/E2M/Model/Ebay/Inventory.php
+++ app/code/local/M2E/E2M/Model/Ebay/Inventory.php
@@ -1,58 +1,12 @@
 <?php
 
-class M2E_E2M_Model_Cron_Task_Ebay_DownloadInventory {
-
-    const CACHE_ID = M2E_E2M_Helper_Data::PREFIX . self::class;
-
-    //########################################
-
-    const MAX_DOWNLOAD_TIME = 946684800;
-    const MAX_REQUESTS = 4;
-    const MAX_DAYS = 118;
-
-    //########################################
-
-    /**
-     * @param DateTime $from
-     * @param DateTime $to
-     *
-     * @return int
-     */
-    private function getProcessAsPercentage(DateTime $from, DateTime $to) {
-
-        $fullInterval = $to->getTimestamp() - self::MAX_DOWNLOAD_TIME;
-        $downloadInterval = $from->getTimestamp() - self::MAX_DOWNLOAD_TIME;
-
-        $percentage = floor(($downloadInterval / $fullInterval) * 100);
-        $percentage > 100 && $percentage = M2E_E2M_Model_Cron_Task_Completed::COMPLETED;
-
-        return $percentage;
-    }
-
-    //########################################
-
-    /**
-     * @inheritDoc
-     */
-    public function completed($taskId, $data) {
-
-        /** @var M2E_E2M_Helper_Data $dataHelper */
-        $dataHelper = Mage::helper('e2m');
-
-        $dataHelper->setConfig(M2E_E2M_Helper_Data::XML_PATH_EBAY_DOWNLOAD_INVENTORY, true, true);
-
-        $dataHelper->logReport($taskId, $dataHelper->__('Finish task of Downloading Inventory from eBay.'));
-
-        Mage::dispatchEvent('m2e_e2m_cron_task_ebay_inventory_download_completed');
-    }
-
-    //########################################
+class M2E_E2M_Model_Ebay_Inventory {
 
     public function updateItem(Mage_Core_Model_Resource $resource, array $item, $id) {
 
         $bind = array();
 
-        $bind['site'] = M2E_E2M_Helper_Data::$MARKETPLACE_CODE[$item['marketplace']];
+        $bind['site'] = M2E_E2M_Helper_Ebay::$MARKETPLACE_CODE[$item['marketplace']];
         $bind['sku'] = $item['identifiers']['sku'];
         $bind['upc'] = $item['identifiers']['upc'];
         $bind['ean'] = $item['identifiers']['ean'];
@@ -374,7 +328,7 @@ class M2E_E2M_Model_Cron_Task_Ebay_DownloadInventory {
 
         $bind = array();
 
-        $bind['site'] = M2E_E2M_Helper_Data::$MARKETPLACE_CODE[$item['marketplace']];
+        $bind['site'] = M2E_E2M_Helper_Ebay::$MARKETPLACE_CODE[$item['marketplace']];
         $bind['ebay_item_id'] = $item['identifiers']['item_id'];
         $bind['sku'] = $item['identifiers']['sku'];
         $bind['upc'] = $item['identifiers']['upc'];
@@ -430,139 +384,10 @@ class M2E_E2M_Model_Cron_Task_Ebay_DownloadInventory {
     //########################################
 
     /**
-     * @inheritDoc
-     * @throws Exception
+     * @param array $items
      */
-    public function process($taskId, $data) {
-
-        /** @var Mage_Core_Helper_Data $coreHelper */
-        $coreHelper = Mage::helper('core');
-
-        /** @var M2E_E2M_Helper_Data $dataHelper */
-        $dataHelper = Mage::helper('e2m');
-
-        /** @var M2E_E2M_Model_Proxy_Ebay_Account $eBayAccount */
-        $eBayAccount = Mage::getSingleton('e2m/Proxy_Ebay_Account');
-
-        /** @var M2E_E2M_Model_Proxy_Ebay_Api $eBayApi */
-        $eBayApi = Mage::getSingleton('e2m/Proxy_Ebay_Api');
-
-        /** @var Mage_Core_Model_Resource $resource */
-        $resource = Mage::getSingleton('core/resource');
-
-        $connWrite = $resource->getConnection('core_write');
-        $cronTasksTableName = $resource->getTableName('m2e_e2m_cron_tasks');
-
-        //----------------------------------------
-
-        $dateTimeZone = new DateTimeZone('UTC');
-        $dateTimeObj = new DateTime('now', $dateTimeZone);
-
-        $fromDateTime = clone $dateTimeObj;
-        $fromDateTime->setTimestamp($data['from']);
-
-        $toDateTime = clone $dateTimeObj;
-        $toDateTime->setTimestamp($data['to']);
-
-        //----------------------------------------
-
-        $request = 0;
-        while ($request < self::MAX_REQUESTS && $fromDateTime->getTimestamp() < $toDateTime->getTimestamp()) {
-
-            $tmpDateTime = clone $fromDateTime;
-            $tmpDateTime->modify('+' . self::MAX_DAYS . ' days');
-
-            //----------------------------------------
-
-            try {
-
-                $response = $eBayApi->sendRequest(array(
-                    'command' => array('inventory', 'get', 'items'),
-                    'data' => array(
-                        'account' => $eBayAccount->getToken(),
-                        'realtime' => true,
-                        'since_time' => $fromDateTime->format('Y-m-d H:i:s'),
-                        'to_time' => $tmpDateTime->format('Y-m-d H:i:s')
-                    )
-                ));
-
-                //----------------------------------------
-
-                if (!empty($response['items'])) {
-                    $items = array();
-                    foreach ($response['items'] as $item) {
-
-                        $fullItemInfo = $eBayApi->sendRequest(array(
-                            'command' => array('item', 'get', 'info'),
-                            'data' => array(
-                                'account' => $eBayAccount->getToken(),
-                                'item_id' => $item['id'],
-                                'parser_type' => 'full',
-                            )
-                        ));
-
-                        $items[$item['id']] = $fullItemInfo['result'];
-                    }
-
-                    if (!empty($items)) {
-                        $items = $this->updateItems($resource, $items);
-                        $this->createItems($resource, $items);
-                    }
-                }
-
-                //----------------------------------------
-
-            } catch (Exception $e) {
-                $dataHelper->logReport($taskId, $e->getMessage(), M2E_E2M_Helper_Data::TYPE_REPORT_ERROR);
-                break;
-            }
-
-            /**
-             ** Dirty hack **
-             *      by pagination responses data
-             * app/code/Component/Ebay/Model/Command/Request/Auth/Trading/Paginated/Serial/Abstract.php:16
-             * getPaginatedOutputData:40
-             */
-            if (!isset($response['to_time'])) {
-                $fromDateTime = $tmpDateTime;
-
-                continue;
-            }
-
-            is_array($response['to_time']) && $response['to_time'] = array_pop($response['to_time']);
-            $toTime = new DateTime($response['to_time'], $dateTimeZone);
-            $fromDateTime = $toTime;
-            !empty($response['items']) && $request++;
-
-            $data['from'] = $fromDateTime->getTimestamp();
-            $connWrite->update($cronTasksTableName, array(
-                'data' => $coreHelper->jsonEncode($data)
-            ), array('instance = ?' => self::class));
-        }
-
-        $process = $this->getProcessAsPercentage($fromDateTime, $toDateTime);
-        $dataHelper->setCacheValue(self::CACHE_ID, $process);
-
-        //----------------------------------------
-
-        $data['from'] = $fromDateTime->getTimestamp();
-
-        $connWrite->update($cronTasksTableName, array(
-            'data' => $coreHelper->jsonEncode($data),
-            'progress' => $dataHelper->getCacheValue(self::CACHE_ID)
-        ), array('instance = ?' => self::class));
-
-        //----------------------------------------
-
-        Mage::dispatchEvent('m2e_e2m_cron_task_ebay_inventory_download_after');
-
-        //----------------------------------------
-
-        return array(
-            'process' => $dataHelper->getCacheValue(self::CACHE_ID, 0),
-            'total' => $dataHelper->getCacheValue(M2E_E2M_Helper_Data::CACHE_ID_EBAY_INVENTORY_TOTAL_COUNT, 0),
-            'variation' => $dataHelper->getCacheValue(M2E_E2M_Helper_Data::CACHE_ID_EBAY_INVENTORY_VARIATION_COUNT, 0),
-            'simple' => $dataHelper->getCacheValue(M2E_E2M_Helper_Data::CACHE_ID_EBAY_INVENTORY_SIMPLE_COUNT, 0)
-        );
+    public function process(array $items) {
+        $items = $this->updateItems(Mage::getSingleton('core/resource'), $items);
+        $this->createItems(Mage::getSingleton('core/resource'), $items);
     }
 }
