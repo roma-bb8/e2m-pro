9af42e94558c08609cdd8b23a3c8c8c2fadad392
roma_bb8 <jungle.romabb8@gmail.com>
2020-04-28 11:53:41 +0300

    v3 ref final

--- delete.sql
+++ delete.sql
@@ -12,3 +12,12 @@ SET FOREIGN_KEY_CHECKS = 1;
 DELETE FROM `core_config_data` WHERE `path` LIKE '%m2e/e2m%';
 
 DELETE FROM `core_resource` WHERE `code` = 'e2m_setup';
+
+-- rm -rf app/code/local/M2E
+-- rm -rf app/design/adminhtml/default/default/template/e2m
+-- rm app/etc/modules/M2E_E2M.xml
+-- rm -rf js/e2m
+-- rm -rf skin/adminhtml/default/default/e2m
+-- rm -rf var/e2m
+-- rm var/locks/ebay_download_inventory.lock
+-- rm var/log/e2m.log
