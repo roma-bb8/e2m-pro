db23296d76e00bc980ca128ffb2d82b6eb5cb5c6
roma_bb8 <jungle.romabb8@gmail.com>
2020-04-28 03:09:49 +0300

    v3 ref final

--- app/code/local/M2E/E2M/Observer/Ebay/StatisticsInventory.php
+++ app/code/local/M2E/E2M/Observer/Ebay/StatisticsInventory.php
@@ -4,26 +4,28 @@ class M2E_E2M_Observer_Ebay_StatisticsInventory {
 
     public function process() {
 
-        $resource = Mage::getSingleton('core/resource');
-        $readConn = $resource->getConnection('core_read');
-        $itemsTableName = $resource->getTableName('m2e_e2m_ebay_items');
-        $itemVariationsTableName = $resource->getTableName('m2e_e2m_ebay_item_variations');
+        $readConn = Mage::getSingleton('core/resource')->getConnection('core_read');
 
-        //----------------------------------------
+        $sql = <<<SQL
+SELECT COUNT(`id`)
+FROM `m2e_e2m_ebay_items`;
+SQL;
+        $total = (int)$readConn->query($sql)->fetchColumn();
 
-        $total = (int)$readConn->select()->from($itemsTableName, 'COUNT(*)')->query()->fetchColumn();
+        $sql = <<<SQL
+SELECT COUNT(`m2e_e2m_ebay_items`.`id`)
+FROM `m2e_e2m_ebay_items`
+         LEFT JOIN `m2e_e2m_ebay_item_variations` on
+    `m2e_e2m_ebay_items`.`id` = `m2e_e2m_ebay_item_variations`.`item_id`
+WHERE `m2e_e2m_ebay_item_variations`.`item_id` IS NULL;
+SQL;
+        $simple = (int)$readConn->query($sql)->fetchColumn();
 
-        $simple = (int)$readConn->select()->from(array('i' => $itemsTableName), 'COUNT(*)')
-            ->join(array('iv' => $itemVariationsTableName), 'i.id = iv.item_id')
-            ->where('iv.item_id IS NULL')
-            ->group('i.ebay_item_id')
-            ->query()->fetchColumn();
-
-        $variation = (int)$readConn->select()->from(array('i' => $itemsTableName), 'COUNT(*)')
-            ->join(array('iv' => $itemVariationsTableName), 'i.id = iv.item_id')
-            ->where('iv.item_id IS NOT NULL')
-            ->group('i.ebay_item_id')
-            ->query()->fetchColumn();
+        $sql = <<<SQL
+SELECT COUNT(DISTINCT `item_id`)
+FROM `m2e_e2m_ebay_item_variations`;
+SQL;
+        $variation = (int)$readConn->query($sql)->fetchColumn();
 
         Mage::helper('e2m/Config')->set(M2E_E2M_Helper_Ebay::XML_PATH_INVENTORY_VARIATION_COUNT, $variation);
         Mage::helper('e2m/Config')->set(M2E_E2M_Helper_Ebay::XML_PATH_INVENTORY_SIMPLE_COUNT, $simple);
